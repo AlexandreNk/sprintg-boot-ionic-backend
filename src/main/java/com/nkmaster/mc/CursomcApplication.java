6abbe49e9c37a3ba7b448eef4397fe6444691760
Alexandre NK <nk-master@uol.com.br>
2021-03-31 08:45:30 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nkmaster/mc/CursomcApplication.java
+++ src/main/java/com/nkmaster/mc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.nkmaster.mc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nkmaster.mc.domain.Categoria;
+import com.nkmaster.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	public CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
