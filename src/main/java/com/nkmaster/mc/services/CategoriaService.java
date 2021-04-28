57ad3dac25fa889a9448217046d1b1791fd8cede
Alexandre NK <nk-master@uol.com.br>
2021-04-28 08:24:29 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/nkmaster/mc/services/CategoriaService.java
+++ src/main/java/com/nkmaster/mc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nkmaster.mc.domain.Categoria;
+import com.nkmaster.mc.dto.CategoriaDTO;
 import com.nkmaster.mc.repositories.CategoriaRepository;
 import com.nkmaster.mc.services.exception.DataIntegrityException;
 import com.nkmaster.mc.services.exception.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(),objDto.getNome());
 	}
 }
