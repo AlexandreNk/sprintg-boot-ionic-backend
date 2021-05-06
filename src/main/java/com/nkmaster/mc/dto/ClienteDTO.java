f7066657b5730faf747c98e055212f8f92caea73
Alexandre NK <nk-master@uol.com.br>
2021-05-06 08:42:04 -0300

    validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/nkmaster/mc/dto/ClienteDTO.java
+++ src/main/java/com/nkmaster/mc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.nkmaster.mc.domain.Cliente;
+import com.nkmaster.mc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
