c115897471ceb0df3cc2e1fbbde978c829adc918
Alexandre NK <nk-master@uol.com.br>
2021-05-20 08:17:10 -0300

    Inserindo Pedido

--- src/main/java/com/nkmaster/mc/domain/PagamentoComCartao.java
+++ src/main/java/com/nkmaster/mc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.nkmaster.mc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.nkmaster.mc.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
